faf138fd37eec35d474fdbaa0bc767e3d16efcb1
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-07-03 12:02:55 +0900

    [add] Batアニメーション / [stop] SwordアニメーションDOTween化

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -15,7 +15,7 @@ public class Sword : MonoBehaviour
     void Update()
     {
         transform.DOLocalRotate(new Vector3(0, 0, 90), 5, mode: RotateMode.WorldAxisAdd);
-        //.SetLoops(1, LoopType.Yoyo)
-        //.SetEase(Ease.InOutQuart);
+        transform.DOLocalMove(new Vector3(0, -5, 0), 5)
+                 .SetLoops(1, LoopType.Yoyo);
     }
 }
